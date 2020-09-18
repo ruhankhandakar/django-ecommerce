69104abdd5daf62e6bdc5a4ec790b8c18243b43b
Ruhan Khandakar <khandakarruhan@gmail.com>
2020-09-18 09:45:29 +0530

    feat: adding api in settings

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
+    'api'
 ]
 
 MIDDLEWARE = [
